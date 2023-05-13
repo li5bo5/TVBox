2ad27d949b96fadf9641c59c4240df336f64a1a2
li5bo5 <li5bo5@163.com>
2023-05-13 17:56:17 +0800

    Update 天龙影视.js

--- ext/天龙影视.js
+++ ext/天龙影视.js
@@ -1,30 +1,16 @@
-var rule = {
-	//title:'cokemv',
-	title:'天龙影视',
-	host:'http://www.85mao.com',
-	url:'/List1/fyclass.html',
-	//url:'/List/fyclass--------fypage---.html',
+var rule={
+    title:'天龙',
+    host:'http://m.85mao.com',
+    // homeUrl:'/',
+    url:'/List/fyfilter.html',
+    searchUrl:'/vodsearch/-------------.html?wd=**',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    
+    filter_url:'{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
 	
-	searchUrl:'/Search/**-------------.html',
-	searchable: 1,//是否启用全局搜索,
-	
-	//分类
-	class_name:'电影&电视剧&综艺&动漫&经典',
-	//class_url:'List1/17&List1/18&List1/20&List1/48&List1/16',
-	class_url:'17&18&20&48&16',
-	//分类筛选,0/1
-	filterable:1,
-	//filter_url:'{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
-	filter_url:'/List/{fl.cateId}}-{{fl.area}}----{{fl.letter}}---fypage---{{fl.year}.html',
-	
-	filter_def:{
-		17:{cateId:'17'},
-		18:{cateId:'18'},
-		20:{cateId:'20'},
-		48:{cateId:'48'},
-		16:{cateId:'16'}
-	},
-	filter: {
+    filter: {
 		"17":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":""},{"n":"动作片","v":"1"},{"n":"喜剧片","v":"2"},{"n":"爱情片","v":"3"},{"n":"科幻片","v":"4"},{"n":"恐怖片","v":"5"},{"n":"剧情片","v":"6"},{"n":"战争片","v":"13"},{"n":"纪录片","v":"14"},{"n":"微电影","v":"19"}]},
 			  {"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"大陆","v":"大陆"},{"n":"香港","v":"香港"},{"n":"台湾","v":"台湾"},{"n":"美国","v":"美国"},{"n":"欧美","v":"欧美"},{"n":"韩国","v":"韩国"},{"n":"日本","v":"日本"},{"n":"日韩","v":"日韩"},{"n":"泰国","v":"泰国"},{"n":"新加坡","v":"新加坡"},{"n":"马来西亚","v":"马来西亚"},{"n":"印度","v":"印度"},{"n":"英国","v":"英国"},{"n":"法国","v":"法国"},{"n":"意大利","v":"意大利"},{"n":"西班牙","v":"西班牙"},{"n":"俄罗斯","v":"俄罗斯"},{"n":"其他","v":"其他"}]},
 			  {"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"},{"n":"2010","v":"2010"},{"n":"2009","v":"2009"},{"n":"2008","v":"2008"},{"n":"2007","v":"2007"},{"n":"2006","v":"2006"},{"n":"2005","v":"2005"}]},
@@ -50,11 +36,27 @@ var rule = {
 			  {"key":"letter","name":"字母","value":[{"n":"全部","v":""},{"n":"A","v":"A"},{"n":"B","v":"B"},{"n":"C","v":"C"},{"n":"D","v":"D"},{"n":"E","v":"E"},{"n":"F","v":"F"},{"n":"G","v":"G"},{"n":"H","v":"H"},{"n":"I","v":"I"},{"n":"J","v":"J"},{"n":"K","v":"K"},{"n":"L","v":"L"},{"n":"M","v":"M"},{"n":"N","v":"N"},{"n":"O","v":"O"},{"n":"P","v":"P"},{"n":"Q","v":"Q"},{"n":"R","v":"R"},{"n":"S","v":"S"},{"n":"T","v":"T"},{"n":"U","v":"U"},{"n":"V","v":"V"},{"n":"W","v":"W"},{"n":"X","v":"X"},{"n":"Y","v":"Y"},{"n":"Z","v":"Z"}]}
 			  ]
 			  },
-
-	detailUrl:'/Movie/fyid.html', //非必填,二级详情拼接链接
-	// https://www.85mao.com/Movie/175104.html
-	// searchUrl:'/index.php/ajax/suggest?mid=1&wd=**',
-	// 搜索:'json:list;name;pic;;id',
-	
-	
+    
+    filter_def:{
+        17:{cateId:'17'},
+        18:{cateId:'18'},
+        20:{cateId:'20'},
+        48:{cateId:'48'}
+    },
+    
+    
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'MOBILE_UA',
+        // "Cookie": "searchneed=ok"
+    },
+    class_parse:'#indexNav li;a&&Text;a&&href;/(\\d+).html',
+    play_parse:true,
+    lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+    limit:6,
+    cate_exclude:'少儿',
+    推荐:'.inner1 li;.box2&&li&&Text;img&&src;.m&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:'#movielist&&li;h3&&Text;img&&src;.info&&Text;a&&href',
+    二级:{"title":".bread&&Text;.cont&&p:eq(2)&&Text","img":"img&&src","desc":";;;.cont&&p:eq(1)&&Text;.cont&&p:eq(0)&&Text","content":".detail-intro&&p&&Text","tabs":"#pagerlist li","lists":"#zongyilist:eq(#id) li"},
+    搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-info-item-content&&Text;a&&href;',
 }
