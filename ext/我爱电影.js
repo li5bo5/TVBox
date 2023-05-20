6f9ceeb6cbd7730d285173e4f14bda6243482bb0
li5bo5 <li5bo5@163.com>
2023-05-20 21:33:56 +0800

    Add files via upload

---
+++ ext/我爱电影.js
@@ -0,0 +1,15 @@
+﻿// 一级筛选页面异常
+muban.首图2.二级.desc = '.data.visible-xs:eq(0)&&Text;;;p.data:eq(-2)&&Text;p.data:eq(-3)&&Text';
+muban.首图2.二级.content = '.detail-content&&Text';
+var rule={
+    title:'我爱电影网',
+    模板:'首图2',
+    host:'https://www.5imv.cc',
+    url:'/vodtype/fyclass-fypage/',
+	searchable:2,//是否启用全局搜索,
+    quickSearch:1,//是否启用快速搜索,
+    searchUrl:'/vodsearch/page/fypage/wd/**/',
+    class_name:'电影&电视剧&综艺&动漫&哔哩',
+    class_url:'1&2&3&4&5',
+    搜索:muban.首图2.搜索1,
+}
