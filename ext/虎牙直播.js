a0cc907f4416a78cd7bb36ade39adcb0c93a4353
li5bo5 <li5bo5@163.com>
2023-11-16 10:08:35 +0800

    Update 虎牙直播.js

--- ext/虎牙直播.js
+++ ext/虎牙直播.js
@@ -31,10 +31,70 @@ var rule = {
     timeout:5000,
     limit:8,
     play_parse:true,
-    // lazy:"js:let rid=input.match(/\\/(\\d+)/)[1];log(rid);let purl=JSON.parse(request('https://mp.huya.com/cache.php?m=Live&do=profileRoom&roomid='+rid)).data.stream.baseSteamInfoList[0].sStreamName;log(purl);let rurl='http://txtest-xp2p.p2p.huya.com/src/'+purl+'.xs?ratio=4000';input={jx:0,url:rurl,parse:0}",
-    lazy:"js:let rid=input.match(/\\/(\\d+)/)[1];function getRealUrl(live_url){let[i,b]=live_url.split('?');let r=i.split('/').pop();let s=r.replace(/\.(flv|m3u8)/,'');let c_tmp=b.split('&').filter(n=>n);let n={};let c_tmp2=[];c_tmp.forEach(function(tmp,index){if(index<3){n[tmp.split('=')[0]]=tmp.split('=')[1]}else{c_tmp2.push(tmp)}});let tmp2=c_tmp2.join('&');n[tmp2.split('=')[0]]=tmp2.split('=')[1];let fm=decodeURIComponent(n.fm).split('&')[0];let u=base64Decode(fm);let p=u.split('_')[0];let f=new Date().getTime()+'0000';let ll=n.wsTime;let t='0';let h=[p,t,s,f,ll].join('_');let m=md5(h);return(i+'?wsSecret='+m+'&wsTime='+ll+'&u='+t+'&seqid='+f+'&'+c_tmp2.pop()).replace('hls','flv').replace('m3u8','flv')}let purl=JSON.parse(request('https://mp.huya.com/cache.php?m=Live&do=profileRoom&roomid='+rid)).data.stream.flv.multiLine[0].url;input={jx:0,url:getRealUrl(purl),parse:0,header:JSON.stringify({'user-agent':'Mozilla/5.0'})}",
-    推荐:'*',
-    一级:'json:data.datas;introduction;screenshot;nick;profileRoom',
+    lazy:`js:
+        let rid = input.match(/\\/ (\\d + ) / )[1];
+        function getRealUrl(live_url) {
+            let [i, b] = live_url.split('?');
+            let r = i.split('/').pop();
+            let s = r.replace(/\.(flv|m3u8)/, '');
+            let c_tmp = b.split('&').filter(n => n);
+            let n = {};
+            let c_tmp2 = [];
+            c_tmp.forEach(function(tmp, index) {
+                if (index < 3) {
+                    n[tmp.split('=')[0]] = tmp.split('=')[1]
+                } else {
+                    c_tmp2.push(tmp)
+                }
+            });
+            let tmp2 = c_tmp2.join('&');
+            n[tmp2.split('=')[0]] = tmp2.split('=')[1];
+            let fm = decodeURIComponent(n.fm).split('&')[0];
+            let u = base64Decode(fm);
+            let p = u.split('_')[0];
+            let f = new Date().getTime() + '0000';
+            let ll = n.wsTime;
+            let t = '0';
+            let h = [p, t, s, f, ll].join('_');
+            let m = md5(h);
+            return (i + '?wsSecret=' + m + '&wsTime=' + ll + '&u=' + t + '&seqid=' + f + '&' + c_tmp2.pop()).replace('hls', 'flv').replace('m3u8', 'flv')
+        }
+        let purl = JSON.parse(request('https://mp.huya.com/cache.php?m=Live&do=profileRoom&roomid=' + rid)).data.stream.flv.multiLine[0].url;
+        input = {
+            jx: 0,
+            url: getRealUrl(purl),
+            parse: 0,
+            header: JSON.stringify({
+                'user-agent': 'Mozilla/5.0'
+            })
+        }
+    `,
+    推荐:`js:
+        let d = [];
+        let jo = JSON.parse(request(input)).data.datas;
+        jo.forEach(it => {
+                d.push({
+                    url: it.profileRoom,
+                    title: it.introduction,
+                    img: it.screenshot,
+                    desc: '👥' + it.totalCount + '👤' + it.nick,
+                })
+        });
+        setResult(d);
+    `,
+    一级:`js:
+        let d = [];
+        let jo = JSON.parse(request(input)).data.datas;
+        jo.forEach(it => {
+                d.push({
+                    url: it.profileRoom,
+                    title: it.introduction,
+                    img: it.screenshot,
+                    desc: '👥' + it.totalCount + '👤' + it.nick,
+                })
+        });
+        setResult(d);
+    `,
     二级:'*',
     搜索:'json:response.3.docs;game_roomName;game_screenshot;game_nick;room_id',
 }
